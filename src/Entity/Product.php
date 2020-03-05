f8dc07abb65275c738b263cb735bdd7e519e6f83
Grégoire <klesk44@gmail.com>
2020-03-05 19:34:26 +0100

    Bug demo

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -2,7 +2,10 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ProductRepository")
@@ -17,14 +20,27 @@ class Product
     private $id;
 
     /**
+     * @Assert\NotNull()
      * @ORM\Column(type="string", length=255)
      */
     private $name;
 
     /**
+     * @Assert\NotNull()
      * @ORM\Column(type="integer")
      */
     private $price;
+
+    /**
+     * @Assert\Valid()
+     * @ORM\OneToMany(targetEntity="App\Entity\Feature", mappedBy="product", cascade={"persist", "remove"})
+     */
+    private $features;
+
+    public function __construct()
+    {
+        $this->features = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -51,6 +67,37 @@ class Product
     public function setPrice(int $price): self
     {
         $this->price = $price;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Feature[]
+     */
+    public function getFeatures(): Collection
+    {
+        return $this->features;
+    }
+
+    public function addFeature(Feature $feature): self
+    {
+        if (!$this->features->contains($feature)) {
+            $this->features[] = $feature;
+            $feature->setProduct($this);
+        }
+
+        return $this;
+    }
+
+    public function removeFeature(Feature $feature): self
+    {
+        if ($this->features->contains($feature)) {
+            $this->features->removeElement($feature);
+            // set the owning side to null (unless already changed)
+            if ($feature->getProduct() === $this) {
+                $feature->setProduct(null);
+            }
+        }
 
         return $this;
     }
